fcf71ee02d2a31173919187ec7dc339e09961d3b
P0tsataja <liis.normann@gmail.com>
2021-12-02 17:12:03 +0000

    Get tasks values from localStorage

--- app.js
+++ app.js
@@ -11,6 +11,43 @@ form.addEventListener("submit", addTask)
 tasksList.addEventListener('click', deleteTask);
 //if the Delete button is clicked, deleteTasks will run
 delTasksBtn.addEventListener('click', deleteTasks);
+//if page is reloaded, run function
+document.addEventListener('DOMContentLoaded', getTasksFromLocalStorage);
+
+//if page is reloaded, also load pre-existing data localStorage (if there is one)
+function getTasksFromLocalStorage() {
+    let tasks;
+    //check if there is an element 'tasks' in local storage, if not then create an array
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function (tasksElement) {
+        //create <li> element
+        const li = document.createElement('li');
+        //add css class
+        li.className = "collection-item";
+        //create a TextNode from the task value
+        const text = document.createTextNode(tasksElement);
+        //add this text as the child of li aka to <li> (in between the html tags where text would go if we were otherwise to fill the list items ourselves)
+        li.appendChild(text);
+        // create link element
+        const link = document.createElement('a');
+        //set href attribute that doesn't lead anywhere (with #)
+        link.setAttribute('href', `#`)
+        //add css style (gap between text and link)
+        link.className = 'secondary-content';
+        //add X text to link
+        link.appendChild(document.createTextNode('X'));
+        //add link to <li>
+        li.appendChild(link);
+        //define the <ul> DOM element by its class
+        const ul = document.querySelector('.collection');
+        //add <li> to <ul>
+        ul.appendChild(li);
+    });
+}
 
 //if X is clicked, the task is removed from the list
 function deleteTask(event) {
@@ -58,7 +95,7 @@ function deleteAllTasksFromLocalStorage() {
 
 function deleteTaskFromLocalStorage(task) {
     let tasks;
-    //check if there is an element 'tasks' in local storage, if not then create an array
+    //check if there is an element 'tasks' in local storage, if not then create an empty array
     if(localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
@@ -83,7 +120,7 @@ function deleteTaskFromLocalStorage(task) {
 function addTask(event) {
     const task = taskInput.value; //gets the input value
 
-    //create <li> DOM element by tag and class name
+    //create <li> element by tag and class name
     const li = document.createElement('li');
     li.className = "collection-item";
     //create a TextNode from the task value
